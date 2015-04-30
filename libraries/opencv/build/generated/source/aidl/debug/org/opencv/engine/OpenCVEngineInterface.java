36a6fada1c54c1c0fab41b44b59b24c715f10b18
Steven Gunanto <gunantosteven@gmail.com>
2015-04-30 19:24:31 +0700

    delete useless library armeabi, mips, x86

--- libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/gunanto/AndroidStudioProjects/FaceRecognitionOpenCV/libraries/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/gunanto/FaceRecognitionOpenCV/libraries/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
